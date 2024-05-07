bbda918083b2c31be884c4b1d373b09ecec4da88
gracedlolu <taiwoowamara@gmail.com>
2024-05-07 04:49:59 +0100

    COMMIT MESSAGE

---
+++ app.js
@@ -0,0 +1,19 @@
+var complexion = "Fair"
+var Myname = "Taiwo"
+console.log(`i am ${complexion} in complexity, + my name is ${Myname}`)
+
+
+
+var course;
+course = "Microbiology"
+console.log(course)
+
+
+let school = "Obafemi_Awolowo_University";
+console.log(school);
+
+let fee = 3500
+console.log(fee)
+
+let age = 5
+console.log(age)
---
+++ script.js
@@ -0,0 +1,8 @@
+console.log(subtrate(9,5)); 7
+function subtrate(Total_buying_point, Total_selling_point){
+    return Total_buying_point - Total_selling_point;
+    
+    let profit = subtrate(9,5);
+    console.log(profit); 
+
+}
